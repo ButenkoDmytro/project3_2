086add18b1a404e90068c1cfd6fec6a9a4289cb1
ButenkoDmytro <137228052+ButenkoDmytro@users.noreply.github.com>
2023-06-21 00:31:50 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int q = 2;
+    while(q <= 1000){
+        bool isPrime = true;
+        for(int w = 2; w < q; w++){
+            if(q % w == 0){
+                isPrime = false;
+                break;
+            }
+        }
+        if(isPrime == true){
+            cout << q << endl;
+        }
+        q++;
+    }
+}
